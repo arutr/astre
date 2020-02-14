c6d5419caac9198540ba33af4faa2d43eaaaa205
Artur Komoter <artur@komoter.pl>
2020-02-14 17:27:19 +0000

    Initial Commit

---
+++ Astre/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Astre
+//
+//  Created by Artur Komoter on 14/02/2020.
+//  Copyright © 2020 Artur Komoter. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
